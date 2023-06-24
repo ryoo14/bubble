9f9a2dcbcf5be9aebec6763b2ffc97cebf35d29b
ryoo14 <anana12185@gmail.com>
2023-06-25 00:41:01 +0900

    refactor: delete unnecessary step

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Bubble from './components/Bubble'
 
 function App() {
   const [input, setInput] = useState("");
   const [bubbles, setBubbles] = useState<string[]>([]);
-  const [seconds, setSeconds] = useState(0);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInput(event.target.value);
